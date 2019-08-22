a61160c35ff74abe12b2196b4c91eb24857f1802
Alex Medvedev <localisseat@gmail.com>
2019-08-22 20:37:37 +0700

    Add project files.

---
+++ RazorPagesMovie/Migrations/20190822133528_ChangePriceDataType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class ChangePriceDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
